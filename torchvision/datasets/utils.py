947b9f9c4fbb14779da213b50630a6fc3ec84255
Forkbabu <sayantandas30011998@gmail.com>
2020-06-06 16:58:32 +0530

    Update utils.py

--- torchvision/datasets/utils.py
+++ torchvision/datasets/utils.py
@@ -10,6 +10,33 @@ import scipy.io as sio
 import torch
 from torch.utils.model_zoo import tqdm
 
+def sampling(proportion, ground_truth):
+    train = {}
+    test = {}
+    labels_loc = {}
+    m = max(ground_truth)
+    for i in range(m):
+        indexes = [j for j, x in enumerate(ground_truth.ravel().tolist()) if x == i + 1]
+        np.random.shuffle(indexes)
+        labels_loc[i] = indexes
+        if proportion != 1:
+            nb_val = max(int((1 - proportion) * len(indexes)), 3)
+        else:
+            nb_val = 0
+        # print(i, nb_val, indexes[:nb_val])
+        # train[i] = indexes[:-nb_val]
+        # test[i] = indexes[-nb_val:]
+        train[i] = indexes[:nb_val]
+        test[i] = indexes[nb_val:]
+    train_indexes = []
+    test_indexes = []
+    for i in range(m):
+        train_indexes += train[i]
+        test_indexes += test[i]
+    np.random.shuffle(train_indexes)
+    np.random.shuffle(test_indexes)
+    return train_indexes, test_indexes
+
 def index_assignment(index, row, col, pad_length):
     new_assign = {}
     for counter, value in enumerate(index):
