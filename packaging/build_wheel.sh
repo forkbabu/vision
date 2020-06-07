8d31acee1aa952dacc927d65ada1d81e665122ef
Forkbabu <sayantandas30011998@gmail.com>
2020-06-07 11:41:04 +0530

    Update build_wheel.sh

--- packaging/build_wheel.sh
+++ packaging/build_wheel.sh
@@ -7,7 +7,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export BUILD_TYPE=wheel
 setup_env 0.6.0
 setup_wheel_python
-pip_install numpy pyyaml future ninja
+pip_install numpy pyyaml future ninja scikit-learn
 setup_pip_pytorch_version
 python setup.py clean
 IS_WHEEL=1 python setup.py bdist_wheel
